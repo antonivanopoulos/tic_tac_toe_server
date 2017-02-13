f294809833cff49af7fb7418337a81b955467f16
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2017-02-13 18:30:49 +1100

    Updates elixir to 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TicTacServer.Mixfile do
   def project do
     [app: :tic_tac_server,
      version: "0.0.2",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
