0513a80bdd7633b92a29c2dd14fb465e41058681
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2017-02-13 18:19:31 +1100

    Bumping version to check out releases

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TicTacServer.Mixfile do
 
   def project do
     [app: :tic_tac_server,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
